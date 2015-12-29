f1477aa427f4aabd698a34aefdd2ee0409b4cd29
Aladeen <zerubeus@gmail.com>
2015-12-29 17:37:17 +0100

    stuff

---
+++ app/src/main/java/com/example/alaeddine/restapp/ActivitySerieDetail.java
@@ -0,0 +1,13 @@
+package com.example.alaeddine.restapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySerieDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_serie_detail);
+    }
+}
